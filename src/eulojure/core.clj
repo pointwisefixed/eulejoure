3d30c9c1f0fec3b40d45f1bb6c14682d79d229b0
Pointwise Fixed <gary@garyrosales.com>
2012-05-20 18:33:27 -0400

    argh, forgetting files

---
+++ src/eulojure/core.clj
@@ -0,0 +1,1 @@
+(ns eulojure.core)
---
+++ test/eulojure/test/core.clj
@@ -0,0 +1,6 @@
+(ns eulojure.test.core
+  (:use [eulojure.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
